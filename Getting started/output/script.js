acbe3998ac7b2484da4bfe7a74d2f9922eedbf6c
jubayerahmmad <zubayersami510@gmail.com>
2025-04-08 11:09:12 +0600

    Generics (added commments for understanding)

--- Getting started/output/script.js
+++ Getting started/output/script.js
@@ -1,63 +1,75 @@
 "use strict";
-// class
+// -----CLASS-------
+// A class is a blueprint for creating objects. It can have properties and methods.
+// Access modifiers (public, private, protected) control the visibility of class members.
 class Player {
     constructor(n, a, c) {
         this.name = n;
         this.age = a;
         this.country = c;
     }
+    // A method to display player details
     play() {
         console.log(`${this.name} is ${this.age} years old and from ${this.country}`);
     }
 }
+// Creating instances of the Player class
 const lamineYamal = new Player("Lamine", 17, "Spain");
 const pedri = new Player("Pedri", 22, "Spain");
+// Storing players in an array
 const players = [];
 players.push(lamineYamal);
 players.push(pedri);
 console.log(players);
-// getters and setters
+// -----GETTERS AND SETTERS-----
+// Getters and setters allow controlled access to private properties.
 class MyClass {
     constructor() {
-        this._myProperty = 0;
+        this._myProperty = 0; // Private property
     }
+    // Getter to retrieve the value of _myProperty
     get myProperty() {
         return this._myProperty;
     }
+    // Setter to update the value of _myProperty
     set myProperty(value) {
         this._myProperty = value;
     }
 }
 const myInstance = new MyClass();
+console.log("instance", myInstance.myProperty); // Accessing via getter
+myInstance.myProperty = 96; // Updating via setter
 console.log("instance", myInstance.myProperty);
-myInstance.myProperty = 96;
-console.log("instance", myInstance.myProperty);
-// array
+// ----ARRAY----
+// Arrays are used to store multiple values of the same type.
 const nums = [1, 4, 2];
-// function
+// ----FUNCTION----
+// Functions are reusable blocks of code. This function returns a number.
 function helloBro() {
     return 5 * 9;
 }
 console.log(helloBro());
-// union
-const data = {};
-// enums
+// -----UNION TYPES------
+// Union types allow a variable to hold values of multiple types.
+const data = {}; // Can be an object or a function
+// -----ENUMS-----
+// Enums are used to define a set of named constants.
 var WeatherConditions;
 (function (WeatherConditions) {
     WeatherConditions[WeatherConditions["Sunny"] = 0] = "Sunny";
     WeatherConditions[WeatherConditions["Rainy"] = 1] = "Rainy";
     WeatherConditions[WeatherConditions["Cloudy"] = 2] = "Cloudy";
 })(WeatherConditions || (WeatherConditions = {}));
-console.log(WeatherConditions.Rainy);
+console.log(WeatherConditions.Rainy); // Outputs: 1
 const examplePerson = {
     fName: "Sam",
     lName: "Altman",
     position: "CEO",
 };
 const add = (x, y) => x + y;
-const substract = (x, y) => x - y;
-console.log(add(4, 6));
-console.log(substract(4, 6));
+const subtract = (x, y) => x - y;
+console.log(add(4, 6)); // Outputs: 10
+console.log(subtract(4, 6)); // Outputs: -2
 class Car {
     start() {
         console.log("Car is Starting....");
@@ -69,3 +81,39 @@ class Car {
 const myCar = new Car();
 myCar.start();
 myCar.stop();
+// ------GENERICS------
+// Generics allow creating reusable components that work with multiple types.
+// Regular functions for specific types
+function printNum(firstVal, secVal) {
+    return [firstVal, secVal];
+}
+function printStr(x, y) {
+    return [x, y];
+}
+function printBoolean(a, b) {
+    return [a, b];
+}
+const num = printNum(36, 45);
+const str = printStr("Hello", "World");
+const bool = printBoolean(true, false);
+console.log(num); // Outputs: [36, 45]
+console.log(str); // Outputs: ["Hello", "World"]
+console.log(bool); // Outputs: [true, false]
+// Generic function to handle multiple data types
+function printUniqueDataType(x, y) {
+    return [x, y];
+}
+const number = printUniqueDataType(69, 45);
+const string = printUniqueDataType("Hello", "World");
+const boolean = printUniqueDataType(true, true);
+console.log(number); // Outputs: [69, 45]
+console.log(string); // Outputs: ["Hello", "World"]
+console.log(boolean); // Outputs: [true, true]
+const dog = printUniqueDataType({ name: "Tom", breed: "Cat" }, { name: "Thomas", breed: "Kitten" });
+console.log(dog);
+// multiple generics params
+function reversedPair(val1, val2) {
+    return [val2, val1];
+}
+const result = reversedPair("Hello", 69);
+console.log(result);
--- Getting started/src/script.ts
+++ Getting started/src/script.ts
@@ -1,8 +1,10 @@
-// class
+// -----CLASS-------
+// A class is a blueprint for creating objects. It can have properties and methods.
+// Access modifiers (public, private, protected) control the visibility of class members.
 class Player {
-  public name: string;
-  public age: number;
-  private country: string; // access modifiers
+  public name: string; // Public: Accessible from anywhere
+  public age: number; // Public: Accessible from anywhere
+  private country: string; // Private: Accessible only within the class
 
   constructor(n: string, a: number, c: string) {
     this.name = n;
@@ -10,6 +12,7 @@ class Player {
     this.country = c;
   }
 
+  // A method to display player details
   play() {
     console.log(
       `${this.name} is ${this.age} years old and from ${this.country}`
@@ -17,46 +20,54 @@ class Player {
   }
 }
 
+// Creating instances of the Player class
 const lamineYamal = new Player("Lamine", 17, "Spain");
 const pedri = new Player("Pedri", 22, "Spain");
 
+// Storing players in an array
 const players: Player[] = [];
 players.push(lamineYamal);
 players.push(pedri);
 console.log(players);
 
-// getters and setters
+// -----GETTERS AND SETTERS-----
+// Getters and setters allow controlled access to private properties.
 class MyClass {
-  private _myProperty: number = 0;
+  private _myProperty: number = 0; // Private property
 
+  // Getter to retrieve the value of _myProperty
   get myProperty(): number {
     return this._myProperty;
   }
 
+  // Setter to update the value of _myProperty
   set myProperty(value: number) {
     this._myProperty = value;
   }
 }
 
 const myInstance = new MyClass();
-
+console.log("instance", myInstance.myProperty); // Accessing via getter
+myInstance.myProperty = 96; // Updating via setter
 console.log("instance", myInstance.myProperty);
-myInstance.myProperty = 96;
-console.log("instance", myInstance.myProperty);
 
-// array
+// ----ARRAY----
+// Arrays are used to store multiple values of the same type.
 const nums: number[] = [1, 4, 2];
 
-// function
+// ----FUNCTION----
+// Functions are reusable blocks of code. This function returns a number.
 function helloBro(): number {
   return 5 * 9;
 }
 console.log(helloBro());
 
-// union
-const data: object | Function = {};
+// -----UNION TYPES------
+// Union types allow a variable to hold values of multiple types.
+const data: object | Function = {}; // Can be an object or a function
 
-// Intersection types
+// ------INTERSECTION TYPES-----
+// Intersection types combine multiple types into one.
 type Person = {
   name: string;
   age: number;
@@ -67,17 +78,19 @@ type Employee = {
   salary: number;
 };
 
-type personAndEmployee = Person & Employee;
+type personAndEmployee = Person & Employee; // Combines Person and Employee types
 
-// enums
+// -----ENUMS-----
+// Enums are used to define a set of named constants.
 enum WeatherConditions {
   Sunny,
   Rainy,
   Cloudy,
 }
-console.log(WeatherConditions.Rainy);
+console.log(WeatherConditions.Rainy); // Outputs: 1
 
-// interface
+// -------INTERFACE--------
+// Interfaces define the structure of an object or a contract for classes.
 interface FamousPerson {
   fName: string;
   lName: string;
@@ -90,15 +103,17 @@ const examplePerson: FamousPerson = {
   position: "CEO",
 };
 
+// Interfaces can also define function types.
 interface MathOperation {
   (x: number, y: number): number;
 }
 const add: MathOperation = (x, y) => x + y;
-const substract: MathOperation = (x, y) => x - y;
+const subtract: MathOperation = (x, y) => x - y;
 
-console.log(add(4, 6));
-console.log(substract(4, 6));
+console.log(add(4, 6)); // Outputs: 10
+console.log(subtract(4, 6)); // Outputs: -2
 
+// Interfaces can be implemented by classes.
 interface Vehicle {
   start(): void;
   stop(): void;
@@ -115,3 +130,54 @@ class Car implements Vehicle {
 const myCar = new Car();
 myCar.start();
 myCar.stop();
+
+// ------GENERICS------
+// Generics allow creating reusable components that work with multiple types.
+
+// Regular functions for specific types
+function printNum(firstVal: number, secVal: number): [number, number] {
+  return [firstVal, secVal];
+}
+function printStr(x: string, y: string): [string, string] {
+  return [x, y];
+}
+function printBoolean(a: boolean, b: boolean): [boolean, boolean] {
+  return [a, b];
+}
+const num = printNum(36, 45);
+const str = printStr("Hello", "World");
+const bool = printBoolean(true, false);
+
+console.log(num); // Outputs: [36, 45]
+console.log(str); // Outputs: ["Hello", "World"]
+console.log(bool); // Outputs: [true, false]
+
+// Generic function to handle multiple data types
+function printUniqueDataType<Type>(x: Type, y: Type): [Type, Type] {
+  return [x, y];
+}
+
+const number = printUniqueDataType<number>(69, 45);
+const string = printUniqueDataType<string>("Hello", "World");
+const boolean = printUniqueDataType<boolean>(true, true);
+console.log(number); // Outputs: [69, 45]
+console.log(string); // Outputs: ["Hello", "World"]
+console.log(boolean); // Outputs: [true, true]
+
+interface Dog {
+  name: string;
+  breed: string;
+}
+
+const dog = printUniqueDataType<Dog>(
+  { name: "Tom", breed: "Cat" },
+  { name: "Thomas", breed: "Kitten" }
+);
+console.log(dog);
+
+// multiple generics params
+function reversedPair<T, U>(val1: T, val2: U): [U, T] {
+  return [val2, val1];
+}
+const result = reversedPair("Hello", 69);
+console.log(result);
