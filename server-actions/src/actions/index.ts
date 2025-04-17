c82ace1e39a4641ecc0e78244c0001c8a382d7ef
jubayerahmmad <zubayersami510@gmail.com>
2025-04-17 22:10:25 +0600

    post data on db

--- server-actions/src/actions/index.ts
+++ server-actions/src/actions/index.ts
@@ -1,10 +1,25 @@
 "use server";
 
+import { db } from "@/utils/prisma";
+
 export const createMovie = async (formData: FormData) => {
-  const title = formData.get("title");
-  const description = formData.get("description");
-  const imageUrl = formData.get("imageUrl");
+  const title = formData.get("title") as string | null;
+  const description = formData.get("description") as string | null;
+  const imageUrl = formData.get("imageUrl") as string | null;
+
+  if (!title || !description || !imageUrl) {
+    throw new Error("Missing required fields: title, description, or imageUrl");
+  }
   console.log("title:", title);
   console.log("description:", description);
   console.log("imageUrl:", imageUrl);
+
+  const newMovie = await db.movie.create({
+    data: {
+      title,
+      description,
+      image: imageUrl,
+    },
+  });
+  console.log(newMovie);
 };
